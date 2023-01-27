d6ed7388ff2cf29bed3ca15867320eab15e412ec
kmj0505 <menge0505@naver.com>
2023-01-28 00:21:37 +0900

    16. 메모리 동적 할당

---
+++ chap16.c
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//예제 16-3
+/*
+int main(void)
+{
+	int* pi;
+	int size = 5;
+	int count = 0;
+	int num;
+	int i;
+
+	pi = (int*)calloc(size, sizeof(int));
+	while (1)
+	{
+		printf("양수만 입력하세요 => ");
+		scanf_s("%d", &num);
+		if (num <= 0) break;
+		if (count == size)
+		{
+			size += 5;
+			pi = (int*)realloc(pi, size * sizeof(int));
+		}
+		pi[count++] = num;
+	}
+	for (i = 0; i < count; i++)
+	{
+		printf("%5d", pi[i]);
+	}
+	free(pi);
+
+	return 0;
+}
+*/
+
+//예제 16-4
+/*
+int main(void)
+{
+	char temp[80];
+	char* str[3];
+	int i;
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("문자열을 입력하세요 : ");
+		gets(temp);
+		str[i] = (char*)malloc(strlen(temp) + 1);
+		strcpy(str[i], temp);
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("%s\n", str[i]);
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		free(str[i]);
+	}
+
+	return 0;
+}
+*/
+
+//예제 16-5
+/*
+void print_str(char** ps);
+
+int main(void)
+{
+	char temp[80];
+	char* str[21] = { 0 };
+	int i = 0;
+
+	while (i < 20)
+	{
+		printf("문자열을 입력하세요 : ");
+		gets(temp);
+		if (strcmp(temp, "end") == 0)break;
+		str[i] = (char*)malloc(strlen(temp) + 1);
+		strcpy(str[i], temp);
+		i++;
+	}
+	print_str(str);
+
+	for (i = 0; str[i] != NULL; i++)
+	{
+		free(str[i]);
+	}
+	return 0;
+}
+
+void print_str(char** ps)
+{
+	while (*ps != NULL)
+	{
+		printf("%s\n", *ps);
+		ps++;
+	}
+}
+*/
