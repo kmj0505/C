16cb45ef68db2c59698fd8b18f31d1a9d5f12622
kmj0505 <menge0505@naver.com>
2023-03-02 23:34:13 +0900

    11. 문자

---
+++ Chap11/Chap11/chap11-4.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+//¿¹Á¦ 11-4
+
+int main(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 3; i++)
+	{
+		scanf("%c", &ch);
+		printf("%c", ch);
+	}
+	return 0;
+}
---
+++ Chap11/Chap11/chap11-5.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//¿¹Á¦ 11-5
+
+int main(void)
+{
+	int res;
+	char ch;
+
+	while (1)
+	{
+		res = scanf("%c", &ch);
+		if (res == EOF)break;
+		printf("%d ", ch);
+	}
+	return 0;
+}
